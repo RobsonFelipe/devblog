83ec843a85451e256b9cda8d4c224bf163f12607
Leonardo Rocha <leonardorocha.limaa@gmail.com>
2020-04-25 17:22:01 -0300

    Primeiro commit utilizando novo template

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'Dev Blog', // Required
-  author: 'Ryan Fitzgerald', // Required
-  description: 'Full-stack Web Developer',
+  author: 'Leonardo Lima, Pedro Rocha e Robson Souza', // Required
+  description: 'Test',
   primaryColor: '#3498db', // Required
   showHeaderImage: true,
   showShareButtons: true,
@@ -10,7 +10,7 @@ module.exports = {
     website: 'https://ryanfitzgerald.ca',
     github: 'https://github.com/ryanfitzgerald',
     twitter: 'https://twitter.com/ryanafitzgerald',
-    linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
+    linkedin: 'https://www.linkedin.com/in/leonardo-vin%C3%ADcius-r-7ba1a6118/',
   },
   pathPrefix: '/devblog',
   siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
