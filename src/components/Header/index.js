e077432fbcf0211e3d6b4cfdbe8f3228a213fe28
Leonardo Rocha <leonardorocha.limaa@gmail.com>
2020-04-25 19:32:57 -0300

    Testando

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,6 +5,7 @@ import Container from '../Container';
 import HeaderImage from '../HeaderImage';
 import Social from '../Social';
 import H1 from '../H1';
+import H2 from '../H2';
 import P from './P';
 import Link from './Link';
 import Wrapper from './Wrapper';
@@ -15,25 +16,25 @@ function Header({ config }) {
   return (
     <Container>
       <Wrapper>
-      <H1><Link to="/">{author}</Link></H1>
-      <P>{description}</P>
       {userConfig.showHeaderImage && (
           <HeaderImage/>
         )}
+      <H1><Link to="/">{author}</Link></H1>
+      <P>{description}</P>
       </Wrapper>
-        <H1><Link to="/">{dev1}</Link></H1>
+        <H2><Link to="/">{dev1}</Link></H2>
         {socialLeo &&
           <Social
             github={socialLeo.github}
           />
         }
-        <H1><Link to="/">{dev2}</Link></H1>
+        <H2><Link to="/">{dev2}</Link></H2>
         {socialPedro &&
           <Social
             github={socialPedro.github}
           />
         }
-        <H1><Link to="/">{dev3}</Link></H1>
+        <H2><Link to="/">{dev3}</Link></H2>
         {socialRobson &&
           <Social
             github={socialRobson.github}
