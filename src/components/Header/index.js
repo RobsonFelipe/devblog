583729579a1eeb8324a3c924d51330980852cf9e
Leonardo Rocha <leonardorocha.limaa@gmail.com>
2020-04-25 22:02:34 -0300

    Teste2 padrão

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,41 +5,30 @@ import Container from '../Container';
 import HeaderImage from '../HeaderImage';
 import Social from '../Social';
 import H1 from '../H1';
-import H2 from '../H2';
 import P from './P';
 import Link from './Link';
 import Wrapper from './Wrapper';
 
 function Header({ config }) {
-  const { author, dev1, dev2, dev3, description, socialLeo, socialPedro, socialRobson } = config;
+  const { author, description, social } = config;
 
   return (
     <Container>
       <Wrapper>
-      {userConfig.showHeaderImage && (
+        {userConfig.showHeaderImage && (
           <HeaderImage/>
         )}
-      <H1><Link to="/">{author}</Link></H1>
-      <P>{description}</P>
+        <H1><Link to="/">{author}</Link></H1>
+        <P>{description}</P>
+        {social &&
+          <Social
+            website={social.website}
+            github={social.github}
+            twitter={social.twitter}
+            linkedin={social.linkedin}
+          />
+        }
       </Wrapper>
-        <H2><Link to="/">{dev1}</Link></H2>
-        {socialLeo &&
-          <Social
-            github={socialLeo.github}
-          />
-        }
-        <H2><Link to="/">{dev2}</Link></H2>
-        {socialPedro &&
-          <Social
-            github={socialPedro.github}
-          />
-        }
-        <H2><Link to="/">{dev3}</Link></H2>
-        {socialRobson &&
-          <Social
-            github={socialRobson.github}
-          />
-        }
     </Container> 
   );
 }
