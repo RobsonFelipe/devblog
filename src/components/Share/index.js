4f4fad18dce54d59210ed654bcbd5a08604cb0c0
Leonardo Rocha <leonardorocha.limaa@gmail.com>
2020-04-25 21:32:09 -0300

    Teste2 padrão

--- src/components/Share/index.js
+++ src/components/Share/index.js
@@ -1,37 +1,13 @@
 import React from 'react';
 
-import H2 from '../H2';
 import Wrapper from './Wrapper';
-import ContactLink from './ShareButton';
+import SocialLink from './SocialLink';
 
-function Share({ title, url }) {
-  const encodedURL = encodeURI(url);
-  const text = encodeURI(title);
+function Share({ github }) {
   return (
     <Wrapper>
-      <H2>Contato</H2>
-      <ContactLink href={`https://twitter.com/intent/tweet?text=${text}&url=${encodedURL}`}>
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" />
-          </svg>
-        
-      </ContactLink>
-      <ContactLink
-        href={`https://twitter.com/intent/tweet?text=${text}&url=${encodedURL}`}
-        target="_blank"
-        aria-label="Share on Twitter"
-        rel="noopener"
-      >
+    {github && (
+      <SocialLink href={github} target="_blank" rel="noopener">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="24"
@@ -43,52 +19,12 @@ function Share({ title, url }) {
           strokeLinecap="round"
           strokeLinejoin="round"
         >
-          <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z" />
+          <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" />
         </svg>
-      </ContactLink>
-      <ContactLink
-        href={`https://www.linkedin.com/shareArticle?mini=true&url=${encodedURL}&title=${text}`}
-        target="_blank"
-        aria-label="Share on LinkedIn"
-        rel="noopener"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
-          <rect x="2" y="9" width="4" height="12" />
-          <circle cx="4" cy="4" r="2" />
-        </svg>
-      </ContactLink>
-      <ContactLink
-        href={`mailto:?subject=${text}&body=${encodedURL}`}
-        aria-label="Share by Email"
-        rel="noopener"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />
-          <polyline points="22,6 12,13 2,6" />
-        </svg>
-      </ContactLink>
-    </Wrapper>
+        Github
+      </SocialLink>
+    )}
+  </Wrapper>
   );
 }
 
