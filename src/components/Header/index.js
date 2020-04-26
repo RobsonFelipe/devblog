d18d58d3b33790e46bf02162db0309140af2f5af
Leonardo Rocha <leonardorocha.limaa@gmail.com>
2020-04-26 16:39:26 -0300

    Teste2 padrão

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,12 +5,13 @@ import Container from '../Container';
 import HeaderImage from '../HeaderImage';
 import Social from '../Social';
 import H1 from '../H1';
+import H2 from '../H2';
 import P from './P';
 import Link from './Link';
 import Wrapper from './Wrapper';
 
 function Header({ config }) {
-  const { author, description, socialLeo } = config;
+  const { author, description, socialLeo, dev1 } = config;
 
   return (
     <Container>
@@ -20,6 +21,7 @@ function Header({ config }) {
         )}
         <H1><Link to="/">{author}</Link></H1>
         <P>{description}</P>
+        <H2><Link to="/">{dev1}</Link></H2>
         {socialLeo &&
           <Social
             github={socialLeo.github}
