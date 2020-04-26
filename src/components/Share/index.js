c8bef52ad47d6b48af3e6b70414df636f5f89b09
Leonardo Rocha <leonardorocha.limaa@gmail.com>
2020-04-25 21:23:14 -0300

    Teste2 padrão

--- src/components/Share/index.js
+++ src/components/Share/index.js
@@ -26,7 +26,7 @@ function Share({ title, url }) {
           </svg>
           Github
       </ContactLink>
-      <ShareButton
+      <ContactLink
         href={`https://twitter.com/intent/tweet?text=${text}&url=${encodedURL}`}
         target="_blank"
         aria-label="Share on Twitter"
@@ -45,8 +45,8 @@ function Share({ title, url }) {
         >
           <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z" />
         </svg>
-      </ShareButton>
-      <ShareButton
+      </ContactLink>
+      <ContactLink
         href={`https://www.linkedin.com/shareArticle?mini=true&url=${encodedURL}&title=${text}`}
         target="_blank"
         aria-label="Share on LinkedIn"
@@ -67,8 +67,8 @@ function Share({ title, url }) {
           <rect x="2" y="9" width="4" height="12" />
           <circle cx="4" cy="4" r="2" />
         </svg>
-      </ShareButton>
-      <ShareButton
+      </ContactLink>
+      <ContactLink
         href={`mailto:?subject=${text}&body=${encodedURL}`}
         aria-label="Share by Email"
         rel="noopener"
@@ -87,7 +87,7 @@ function Share({ title, url }) {
           <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />
           <polyline points="22,6 12,13 2,6" />
         </svg>
-      </ShareButton>
+      </ContactLink>
     </Wrapper>
   );
 }
