7ebf4e8952b163b17b327770b6719f0c50c057c8
RobsonFelipe <robson2009felipe@gmail.com>
2020-05-31 22:01:26 -0300

    Remoção do serviço DISQUS

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -72,23 +72,6 @@ class BlogPostTemplate extends React.Component {
             )}
           </PageNav>
         </Container>
-        <div id="disqus_thread"></div>
-          <script>
-          
-          var disqus_config = function () {
-            this.page.url = 'https://www.blogpiiunifil.com/'; 
-            this.page.identifier = 'postagens'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-          };
-          
-          (function() { // DON'T EDIT BELOW THIS LINE
-            var d = document, s = d.createElement('script');
-            s.src = 'https://https-www-blogpiiunifil-com.disqus.com/embed.js';
-            s.setAttribute('data-timestamp', +new Date());
-            (d.head || d.body).appendChild(s);
-          })();
-          </script>
-<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-             
       </Layout>
     );
   }
